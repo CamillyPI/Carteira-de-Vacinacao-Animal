d26681eeb9a97ef7cdcda83a3adc890379f1aceb
Camilly Imperialos <pachecocamillyi@gmail.com>
2025-06-10 21:47:51 -0300

    Adição do atributo da FK

--- moldes/Animal.py
+++ moldes/Animal.py
@@ -1,11 +1,12 @@
 class Animal:
         
-    def __init__(self, nome, idade, especie, raca, id=None):
+    def __init__(self, nome, idade, especie, raca, pessoa_id, id=None):
         self.id = id
         self.nome = nome
         self.idade = idade
         self.especie = especie
         self.raca = raca
+        self.pessoa_id = pessoa_id
 
 
     def __str__(self):
--- moldes/Vacina.py
+++ moldes/Vacina.py
@@ -1,10 +1,11 @@
 class Vacina:
         
-    def __init__(self, nome, dose, data, id=None):
+    def __init__(self, nome, dose, data, animal_id, id=None):
         self.id = id
         self.nome = nome
         self.dose = dose
         self.data = data
+        self.animal_id = animal_id
 
 
     def __str__(self):
